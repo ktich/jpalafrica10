9ea773cea538a40b280d07ca69db93090f09022e
Kim Tichmann <Kim@Jpals-MacBook-Air.local>
2020-02-11 20:43:18 +0200

    timeline & nav

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ $(function(){
     $().timelinr();
  });
 
-$(function() {
-    var x = $("#myTopnav")
+function myFunction() {
+    var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
       x.className = "topnav";
     }
-});
+  }
