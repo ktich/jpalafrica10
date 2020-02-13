088ec4ec3c844b7a39b108376602e9cf4aabfd63
Kim Tichmann <Kim@Jpals-MacBook-Air.local>
2020-02-13 11:48:58 +0200

    adding map, etc.

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+$(".our-work").on('click', function(event){
+  
+    $(".our-work").each(function(i){
+      var currentElement = '#'+$(".our-work").get(i).id;
+      $(currentElement).css("fill", "#e35925");
+    });
+    
+    $(".content").each(function(i){
+      var currentContent = $(".content").get(i);
+      $(currentContent).css("display", "none");
+    })
+    
+    var countryId =  '#'+event.target.id;
+    var textClass = '.'+event.target.id;
+    $(countryId).css("fill", "black");
+    $(textClass).css("display", "block");
+  })
+});
+  
