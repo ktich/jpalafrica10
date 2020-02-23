e706d72b1fcccf532b5a90501e832376801b3e20
Kim Tichmann <Kim@Jpals-MacBook-Air.local>
2020-02-23 19:48:16 +0200

    adding real content & changing styles

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 $(document).ready(function(){
 $(".our-work").on('click', function(event){
   
+  //change all other countries to regular background
     $(".our-work").each(function(i){
       var currentElement = '#'+$(".our-work").get(i).id;
-      $(currentElement).css("fill", "#e35925");
+      $(currentElement).css("fill", "#f2c200");
     });
     
+    //display no content in .detail box
     $(".content").each(function(i){
       var currentContent = $(".content").get(i);
       $(currentContent).css("display", "none");
     })
     
+    //change the clicked country and display corresponding content
     var countryId =  '#'+event.target.id;
     var textClass = '.'+event.target.id;
-    $(countryId).css("fill", "black");
+    $(countryId).css("fill", "rgba(0,0,0,0.5)");
     $(textClass).css("display", "block");
     $(".country-click").css("display","none");
   })
